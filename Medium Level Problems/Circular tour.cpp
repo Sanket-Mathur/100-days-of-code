6a0d7c5291fd7ea4adced7682df4554f79cf5e69
Sanket-Mathur <rajeev.sanket@gmail.com>
2021-08-28 00:01:26 +0530

    Day 51 - 5 Problems

---
+++ Medium Level Problems/Circular tour.cpp
@@ -0,0 +1,65 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct petrolPump
+{
+    int petrol;
+    int distance;
+};
+
+
+ // } Driver Code Ends
+/*
+The structure of petrolPump is 
+struct petrolPump
+{
+    int petrol;
+    int distance;
+};*/
+
+/*You are required to complete this method*/
+class Solution{
+  public:
+  
+    //Function to find starting point where the truck can start to get through
+    //the complete circle without exhausting its petrol in between.
+    int tour(petrolPump p[],int n)
+    {
+        int start = 0, petrol = 0, lack = 0;
+        for (int i = 0; i < n; ++i) {
+            petrol = petrol + p[i].petrol - p[i].distance;
+            if (petrol < 0) {
+                start = i + 1;
+                lack = lack - petrol;
+                petrol = 0;
+            }
+        }
+        
+        if (petrol - lack > 0) {
+            return start;
+        } 
+        return -1;
+    }
+};
+
+
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        petrolPump p[n];
+        for(int i=0;i<n;i++)
+            cin>>p[i].petrol>>p[i].distance;
+        Solution obj;
+        cout<<obj.tour(p,n)<<endl;
+    }
+}
+  // } Driver Code Ends
